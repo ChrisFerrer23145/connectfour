579a33e02209df2a9e4a1e8539af8a1808a8bc73
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-12-17 06:14:14 -0500

    Update shiftleft.java

--- shiftleft.java
+++ shiftleft.java
@@ -7,13 +7,13 @@ public class shiftleft {
             }
         }
         
-        for(int i=0; i<7; i++){
+        for(int i=0; i<6; i++){
             String last = newBoard[row][6];
-                if(newBoard[row].length >1) {
-                    for(int j = newBoard[row].length-2; j >= 0; j--){
-                        newBoard[row][j+1] = newBoard[row][j];
-                    }
+            if(newBoard[row].length >1) {
+                for(int j = newBoard[row].length-2; j >= 0; j--){
+                    newBoard[row][j+1] = newBoard[row][j];
                 }
+            }
             newBoard[row][0] = last;
         }
         
